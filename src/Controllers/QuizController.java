26aaaedc6ab0088f33ec5ffac989ca2db430a44b
matik802 <mati88846@gmail.com>
2024-01-22 18:03:26 +0100

    start

--- src/Controllers/QuizController.java
+++ src/Controllers/QuizController.java
@@ -1,14 +1,27 @@
 package Controllers;
 import java.util.List;
 
-import javax.swing.JFrame;
-
 import Models.Question;
+import Views.QuizView;
 
 public class QuizController {
     private QuizState quizState;
     private List<Question> questionsPool;
-    private JFrame quizView;
+    private QuizView quizView;
+    private String language;
     
-    
+    public QuizController(QuizView quizView) {
+        this.quizView = quizView;
+        //pobieranie z bazy
+    }
+
+    public void getNextQuestion() {
+           
+    }
+
+    public void showResult() {
+
+    }
+
+
 }
---
+++ src/Utils/Constants.java
@@ -0,0 +1,6 @@
+package Utils;
+
+public class Constants {
+    public static final String languagePl = "PL";
+    public static final String languageEng = "ENG";
+}
